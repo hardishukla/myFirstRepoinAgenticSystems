6bd31f2e9a59d0767b7ac978acbc2a8ba404db20
hardishukla <155943005+hardishukla@users.noreply.github.com>
2026-02-04 22:06:36 +0530

    Add files via upload

---
+++ python-loops-assignment/Password Retry System.py
@@ -0,0 +1,4 @@
+password=input()
+while password=="admin123":
+    print("Access granted")
+    break
---
+++ python-loops-assignment/Sum Until Zero.py
@@ -0,0 +1,8 @@
+x=0
+while True:
+    num=int(input())
+    if num==0:
+        break
+    else:
+        x+=num
+print(f"Total:{x}")
